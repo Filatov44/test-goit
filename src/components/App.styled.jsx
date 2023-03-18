9715c00452eff2bdf53b8c2260b5683d741eba88
Sergey Filatov <filatov300@gmail.com>
2023-03-18 17:13:49 +0200

    add app styled

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: azure;
+	width: 100vw;
+	height: 100vh;
+`;
